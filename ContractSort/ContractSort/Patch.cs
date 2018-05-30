71d4306eb82e74c1568145b1b50cf9c85ba0fe68
Morphyum <zion@c-schwabe.de>
2018-05-30 22:06:07 +0200

    *v0.1

--- ContractSort/ContractSort/Patch.cs
+++ ContractSort/ContractSort/Patch.cs
@@ -1,20 +1,39 @@
 ﻿using BattleTech;
+using BattleTech.Framework;
+using BattleTech.UI;
 using Harmony;
 using System;
-using System.Collections.Generic;
 
 namespace ContractSort {
 
-    [HarmonyPatch(typeof(Contract), "GenerateSalvage")]
-    public static class Contract_GenerateSalvage_Patch {
-        static void Prefix(Contract __instance, List<UnitResult> enemyMechs, List<VehicleDef> enemyVehicles, List<UnitResult> lostUnits, bool logResults = false) {
+    [HarmonyPatch(typeof(SGContractsWidget), "GetContractComparePriority")]
+    public static class SGContractsWidget_GetContractComparePriority_Patch {
+        static bool Prefix(SGContractsWidget __instance, ref int __result, Contract contract) {
             try {
-               
+                int result = 100;
+                SimGameState Sim = (SimGameState)ReflectionHelper.InvokePrivateMethode(__instance, "get_Sim", null);
+                if (Sim.ContractUserMeetsReputation(contract)) {
+                    if (contract.Override.contractDisplayStyle == ContractDisplayStyle.BaseCampaignRestoration) {
+                        result = 0;
+                    }
+                    else if (contract.Override.contractDisplayStyle == ContractDisplayStyle.BaseCampaignStory) {
+                        result = 1;
+                    }
+                    else if (contract.TargetSystem.Replace("starsystemdef_","").Equals(Sim.CurSystem.Name)) {
+                        result = contract.Override.GetUIDifficulty() + 1;
+                    }
+                    else {
+                        result = contract.Override.GetUIDifficulty() + 11;
+                    }
+                } else {
+                    result = contract.Override.GetUIDifficulty() + 21;
+                }            
+                __result = result;
+                return false;
             }
             catch (Exception e) {
                 Logger.LogError(e);
                 return false;
-
             }
         }
     }
